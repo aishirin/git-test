ef71022873edd84038639646805ffc585190c98e
aishirin <awrnjoomartova@gmail.com>
2022-04-11 17:56:12 +0600

    Added basket

--- minishop/index.js
+++ minishop/index.js
@@ -1,24 +1,42 @@
 console.log(data)
-
+// let basket=[]
 function App() {
+    const [basket,setBasket]=React.useState([])
+    
+    // const buttons=[]
+    // for(let i=0;i<data.length;i++){
+    //     buttons.push(
+    //         <button className="product">
+    //           <div>{data[i].title}</div>
+    //           <div>{data[i].price}</div>
+    //         </button>
+    //     )
+    // }
+
+    let count=0
+basket.map((item)=>count+=item.price)
+console.log(count);
   return (
     <div className="App">
       <div className="product-list">
-        <button className="product" >
-          <div>Название</div>
-          <div>Цена c</div>
-        </button>
+     {data.map((item)=>(
+        <button className="product" key={item.id} onClick={()=>setBasket([...basket,item])}>
+        <div>{item.title}</div>
+        <div>{item.price}</div>
+      </button>
+     ))}
       </div>
       <div className="basket">
         <div className="total">
           <span>Итого:</span>
-          <span>0 c</span>
+          <span>{`${count} сом`}</span>
         </div>
         <div>
-          <button className="product" >
-            <div>Название</div>
-            <div>Цена c</div>
-          </button>
+          {basket.map((item,i)=>(
+          <button className="product" key={item.id} onClick={()=>setBasket(basket.splice(item,1))}>
+            <div>{item.title}</div>
+            <div>{`${item.price} c`}</div>
+          </button>))}
         </div>
       </div>
     </div>
